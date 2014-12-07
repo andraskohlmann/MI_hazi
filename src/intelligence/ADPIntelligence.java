7c3f357d6a904534039e569e3ce6c117c962c933
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-07 01:40:14 +0100

    Started filling ADPIntelligence

--- src/intelligence/ADPIntelligence.java
+++ src/intelligence/ADPIntelligence.java
@@ -1,13 +1,27 @@
 package intelligence;
 
+import environment.Activity;
 import environment.CarState;
+import environment.Result;
 
 public class ADPIntelligence {
 	
-	private CarState state;
 	private double[][][] T;
+	private double[] R;
+	private double[] U;
+	private double[][] Nsa;
+	private double[][][] Nsas;
+	
+	private CarState prevoiusState;
+	private Activity prevoiusActivity;
 	
 	public ADPIntelligence() {
+		T = new double[CarState.maxState()][3][CarState.maxState()];
+		R = new double[CarState.maxState()];
+	}
+	
+	Activity move(Result result) {
+		return Activity.neutral;
 	}
 
 }

fe869fc382494c0d8e25680cace9eace33234725
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-07 01:11:09 +0100

    Filled Result class.

--- src/environment/Result.java
+++ src/environment/Result.java
@@ -1,4 +1,20 @@
 package environment;
 
 public class Result {
+	
+	private CarState state;
+	private double award;
+	
+	public Result(CarState s, double a) {
+		state = s;
+		award = a;
+	}
+	
+	public CarState getState() {
+		return state;
+	}
+	
+	public double getAward() {
+		return award;
+	}
 }

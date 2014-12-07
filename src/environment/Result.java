a5c8058ff2655cf858ec893564b05024b4a2c74f
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-07 02:03:42 +0100

    Reward not award -.-'

--- src/environment/Result.java
+++ src/environment/Result.java
@@ -1,20 +1,20 @@
 package environment;
 
 public class Result {
-	
+
 	private CarState state;
 	private double reward;
-	
+
 	public Result(CarState s, double a) {
 		state = s;
 		reward = a;
 	}
-	
+
 	public CarState getState() {
 		return state;
 	}
-	
-	public double getAward() {
+
+	public double getReward() {
 		return reward;
 	}
 }

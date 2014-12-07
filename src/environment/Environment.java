92bc2d11c2cf3df85f34dcbdbf0bb8c903190221
m2kohand <uh.liamg@gmail.com>
2014-12-07 02:03:41 +0100

    Environment thing
    Thingy stuff done on the thing

--- src/environment/Environment.java
+++ src/environment/Environment.java
@@ -1,7 +1,26 @@
 package environment;
 
 public class Environment {
-	
-	
-//	private Result thingsHappen(Ca)
+
+	private static final double winPosition = 1.75 * Math.PI;
+	private static final double winReward = 1;
+	private static final double constReward = -0.01;
+
+	private static final double gravity = 9.81;
+	private static final double deltaTime = 0.01;
+	private static final double accelerationRatio = 5;
+
+	public Result getResult(CarState state_t, Activity a) {
+		CarState state_tpp = calculateNextState(state_t, a);
+		double reward = state_tpp.getPosition() == winPosition ? winReward
+				: constReward;
+		return new Result(state_tpp, reward);
+	}
+
+	private CarState calculateNextState(CarState state_t, Activity a) {
+		double m = -Math.sin(state_t.getPosition());
+		double acc = Math.sin(Math.atan(m)) * gravity + (a.ordinal()-1) * accelerationRatio;
+		double vel = (state_t.getVelocity() + acc / deltaTime) / 2;
+		return new CarState(state_t.getPosition() + vel / deltaTime, vel);
+	}
 }

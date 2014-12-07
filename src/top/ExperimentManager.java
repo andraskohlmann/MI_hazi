4cfd871e9d72d34cc73cd955b37964a75dc9e430
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-07 23:09:25 +0100

    Finished ExperimentManager
    (aka Miss Candy, with or without clothes. preferred without.)

--- src/top/ExperimentManager.java
+++ src/top/ExperimentManager.java
@@ -1,14 +1,33 @@
 package top;
 
+import intelligence.ADPIntelligence;
+import environment.Activity;
+import environment.CarState;
 import environment.Environment;
+import environment.Result;
 
 public class ExperimentManager {
 
-	/**
-	 * @param args
-	 */
+	public static int maxIteration = 1000;
+
 	public static void main(String[] args) {
 		Environment environment = new Environment();
+		Activity[] pi = null;
+
+		for (int j = 0; j < 10; j++) {
+			ADPIntelligence ai = new ADPIntelligence(
+					pi == null ? ADPIntelligence.startPi() : pi, new CarState(
+							3 * Math.PI / 4, 0), Activity.neutral);
+			Result result = environment.getResult(new CarState(3 * Math.PI / 4,
+					0), Activity.neutral);
+
+			for (int i = 0; i < maxIteration; i++) {
+				Activity act = ai.move(result);
+				result = environment.getResult(result.getState(), act);
+			}
+
+			pi = ai.nextPi();
+		}
 	}
 
 }

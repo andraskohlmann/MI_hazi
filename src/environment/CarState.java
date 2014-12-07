9efb8948cb7c219229c181417268962a2170684f
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-07 02:02:20 +0100

    Set functions to public visibility

--- src/environment/CarState.java
+++ src/environment/CarState.java
@@ -1,69 +1,75 @@
 package environment;
 
 public class CarState {
-	
-//	public static void main(String[] args) {
-//		CarState s = new CarState(6.2, 1);
-//		System.out.println(s.getStateNum());
-//		CarState n = new CarState(s.getStateNum());
-//		System.out.println(n.position);
-//		System.out.println(n.velocity);
-//	}
-	
+
+	// public static void main(String[] args) {
+	// CarState s = new CarState(6.2, 1);
+	// System.out.println(s.getStateNum());
+	// CarState n = new CarState(s.getStateNum());
+	// System.out.println(n.position);
+	// System.out.println(n.velocity);
+	// }
+
 	private static final double minPosition = 0;
 	private static final double maxPosition = 2 * Math.PI;
-	
+
 	private static final double minVelocity = -1;
 	private static final double maxVelocity = 1;
-	//TODO
-	
+	// TODO
+
 	private static final int positionResolution = 11;
 	private static final int velocityResolution = 11;
-	
+
 	public static int maxState() {
 		return positionResolution * velocityResolution;
 	}
-	
+
 	private double position;
 	private double velocity;
-	
-	CarState(int s) {
+
+	public CarState(int s) {
 		setStateNum(s);
 	}
-	
-	CarState(double p, double v) {
+
+	public CarState(double p, double v) {
 		position = p;
 		velocity = v;
 	}
-	
-	double getPosition() {
+
+	public double getPosition() {
 		return position;
 	}
-	
-	double getVelocity() {
+
+	public double getVelocity() {
 		return velocity;
 	}
-	
-	int getStateNum() {
-		double normalPosition = (position - minPosition) / (maxPosition - minPosition);
-		int discretePosition = (int) (Math.round(normalPosition * (positionResolution - 1)));
-		
-		double normalVelocity = (velocity - minVelocity) / (maxVelocity - minVelocity);
-		int discreteVelocity = (int) (Math.round(normalVelocity * (velocityResolution - 1)));
-		
-//		System.out.println(discretePosition);
-//		System.out.println(discreteVelocity);
-		
-		return discretePosition * (velocityResolution) + discreteVelocity;		
+
+	public int getStateNum() {
+		double normalPosition = (position - minPosition)
+				/ (maxPosition - minPosition);
+		int discretePosition = (int) (Math.round(normalPosition
+				* (positionResolution - 1)));
+
+		double normalVelocity = (velocity - minVelocity)
+				/ (maxVelocity - minVelocity);
+		int discreteVelocity = (int) (Math.round(normalVelocity
+				* (velocityResolution - 1)));
+
+		// System.out.println(discretePosition);
+		// System.out.println(discreteVelocity);
+
+		return discretePosition * (velocityResolution) + discreteVelocity;
 	}
-	
-	void setStateNum(int num) {
+
+	public void setStateNum(int num) {
 		int discreteVelocity = num % velocityResolution;
-		double normalVelocity = (double) discreteVelocity / (velocityResolution - 1);
+		double normalVelocity = (double) discreteVelocity
+				/ (velocityResolution - 1);
 		velocity = minVelocity + normalVelocity * (maxVelocity - minVelocity);
-		
+
 		int discretePosition = num / velocityResolution;
-		double normalPosition = (double) discretePosition / (positionResolution - 1);
+		double normalPosition = (double) discretePosition
+				/ (positionResolution - 1);
 		position = minPosition + normalPosition * (maxPosition - minPosition);
 	}
 
@@ -71,8 +77,7 @@ public class CarState {
 	public boolean equals(Object o) {
 		if (o instanceof CarState) {
 			return (getStateNum() == ((CarState) o).getStateNum());
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
@@ -81,5 +86,5 @@ public class CarState {
 	public int hashCode() {
 		return getStateNum();
 	}
-	
+
 }

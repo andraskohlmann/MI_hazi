bd5a263ca1bce428ecbe8cb6d7fe6bf27f7aa3c0
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-07 01:23:29 +0100

    First touches of AI

---
+++ src/intelligence/ADPIntelligence.java
@@ -0,0 +1,13 @@
+package intelligence;
+
+import environment.CarState;
+
+public class ADPIntelligence {
+	
+	private CarState state;
+	private double[][][] T;
+	
+	public ADPIntelligence() {
+	}
+
+}

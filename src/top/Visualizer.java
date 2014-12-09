ba1d7e8437f990820994991348b1adf2e8868c57
Martinek Vilmos <vilmos.martinek@gmail.com>
2014-12-09 02:44:58 +0100

    Matlab does not die :+1:

--- src/top/Visualizer.java
+++ src/top/Visualizer.java
@@ -5,8 +5,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -15,69 +16,67 @@ import environment.Activity;
 import environment.CarState;
 import environment.Environment;
 
-public class Visualizer extends JFrame implements ActionListener, MouseListener {
+public class Visualizer extends JFrame implements ActionListener, MouseListener, WindowListener {
 
 	private static final long serialVersionUID = 1L;
 	private Timer timer;
 	private GraphicPanel gfx;
 	private int stepCount;
-	
+
 	private CarState start;
-	
+
 	private JPanel panel;
 	private CardLayout layout;
-	
+
 	Visualizer(Environment e, CarState s) {
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setLocation(100, 100);
 		setSize(1260, 720);
 		setTitle("Experiment Visualizer");
 		setResizable(false);
-		
+
 		addMouseListener(this);
-		
+		addWindowListener(this);
+
 		panel = new JPanel();
-		
+
 		layout = new CardLayout();
 		panel.setLayout(layout);
-		
+
 		gfx = new GraphicPanel(e, s);
 		start = s;
-		
+
 		panel.add(new FrontPanel(this), "front");
 		panel.add(gfx, "show");
-		
+
 		add(panel);
-		
+
 		layout.show(panel, "front");
-		
+
 		stepCount = 0;
-		
-		setVisible(true);
 	}
-	
+
 	public void start(Activity[] p) {
 		timer = new Timer(50, this);
-		
+
 		gfx.setPi(p);
-		
+
 		layout.show(panel, "show");
 		timer.start();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if(arg0.getActionCommand() == null) {
+		if (arg0.getActionCommand() == null) {
 			stepCount++;
 			if (gfx.step() == 1 || stepCount > 1000) {
 				timer.stop();
 				timer = null;
 				gfx.setStopped();
 			}
-			
+
 			repaint();
-		}
-		else if (arg0.getActionCommand().equals("start")) {
+		} else if (arg0.getActionCommand().equals("start")) {
 			start(ExperimentManager.experiment());
 		}
 	}
@@ -86,7 +85,7 @@ public class Visualizer extends JFrame implements ActionListener, MouseListener 
 	public void mouseClicked(MouseEvent arg0) {
 		stepCount = 0;
 		gfx.setResult(start);
-		
+
 		if (timer == null) {
 			timer = new Timer(50, this);
 			timer.start();
@@ -94,7 +93,7 @@ public class Visualizer extends JFrame implements ActionListener, MouseListener 
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent arg0) {	
+	public void mouseEntered(MouseEvent arg0) {
 	}
 
 	@Override
@@ -108,5 +107,34 @@ public class Visualizer extends JFrame implements ActionListener, MouseListener 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 	}
-	
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		MathLabGraph.disconnect();
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+	}
+
 }
